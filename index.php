70b6e6a9389fedc2d3bea10ae6dc1d9a9d235da4
root <root@master1.hkg-e7db.internal>
2019-08-04 16:40:50 +0000

    testtest

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
 
 <head>
-<title>Hello World! RayRayRayRayRayRayRayRayRayRay</title>
+<title>Hello World!</title>
 </head>
 
 <body>
 
-<?php echo "Hello World!"; ?>
+<?php echo "Hello World! Ray Ray"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
